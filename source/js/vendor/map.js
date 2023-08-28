6715ff45bc02ce5ebaab67a43f990e27d0a5004b
IrinaLemeshkina <oleksinairina@gmail.com>
2023-08-28 11:33:20 +0300

    map adress correct

--- source/js/vendor/map.js
+++ source/js/vendor/map.js
@@ -1,9 +1,9 @@
-let map = L.map('map').setView([59.968331, 30.317547], 17);
+let map = L.map('map').setView([55.774836, 37.632664], 17);
 let leaflet = L.icon({
   iconUrl: './img/marker-icon.png',
   iconSize: [38, 50],
   iconAnchor: [19, 50],
 });
-let marker = L.marker([59.968331, 30.317547], { icon: leaflet }).addTo(map);
+let marker = L.marker([55.774836, 37.632664], { icon: leaflet }).addTo(map);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?{foo}', { foo: 'bar' }).addTo(map);
