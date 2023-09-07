ff7a138f8dfa8ccebc5598faacf8ab1bdb519a5f
IrinaLemeshkina~ <oleksinairina@gmail.com>
2023-09-07 15:37:32 +0300

    Исправляю баг с картой

--- source/js/modules/map.js
+++ source/js/modules/map.js
@@ -1,9 +1,17 @@
-let map = L.map('map').setView([55.774836, 37.632664], 17);
-let leaflet = L.icon({
-  iconUrl: './img/marker-icon.png',
-  iconSize: [38, 50],
-  iconAnchor: [19, 50],
-});
-let marker = L.marker([55.774836, 37.632664], { icon: leaflet }).addTo(map);
+// Проверяю наличие контейнера для карты на странице
+const mapContainer = document.getElementById('map');
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?{foo}', { foo: 'bar' }).addTo(map);
+// Добавляю карту, если контейнер найден
+if (mapContainer) {
+    let map = L.map('map').setView([55.774836, 37.632664], 17);
+    let leaflet = L.icon({
+      iconUrl: './img/marker-icon.png',
+      iconSize: [38, 50],
+      iconAnchor: [19, 50],
+    });
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?{foo}', { foo: 'bar' }).addTo(map);
+
+    // Добавляю маркер 
+    let marker = L.marker([55.774836, 37.632664], { icon: leaflet }).addTo(map);
+}
